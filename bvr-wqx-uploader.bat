bb6afb4fa9bb1478642638d918ec513f81f16b48
InigoP <inigop@rogers.com>
2023-11-20 18:55:37 -0800

    added pause

--- bvr-wqx-uploader.bat
+++ bvr-wqx-uploader.bat
@@ -1,4 +1,4 @@
 "C:/Program Files/R/R-4.2.1/bin/x64/RScript.exe" install-deps.R
 "C:/Program Files/R/R-4.2.1/bin/x64/RScript.exe" python-setup.R
 "C:/Program Files/R/R-4.2.1/bin/x64/R.exe" -e "shiny::runApp('.', launch.browser = TRUE)"
-
+pause
