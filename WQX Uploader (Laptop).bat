eafc6b8bd241f2ae640c6e7ddb3fbc18d3f66f65
InigoP <inigop@rogers.com>
2023-12-07 14:04:10 -0800

    updated R pathing

--- WQX Uploader (Laptop).bat
+++ WQX Uploader (Laptop).bat
@@ -1,4 +1,4 @@
-set RPath=C:\Program Files\R\R-4.2.1\bin
+set RPath=C:\Users\%USERPROFILE%\AppData\Local\Programs\R\R-4.3.2\bin
 
 "%RPath%\RScript.exe" install-deps.R
 "%RPath%\RScript.exe" python-setup.R
