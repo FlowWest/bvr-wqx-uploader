cb136dd07750b81832d1234c2c78a61fb644bc62
InigoP <inigop@rogers.com>
2024-01-03 20:36:17 -0800

    remove git related commands

--- WQX Uploader (Desktop).bat
+++ WQX Uploader (Desktop).bat
@@ -2,35 +2,6 @@
 SETLOCAL
 setlocal enabledelayedexpansion
 
-REM Function to find the Git executable
-FOR %%I IN (
-    "C:\Program Files\Git\cmd\git.exe"
-    "C:\Program Files\Git\bin\git.exe"
-    "C:\Program Files\Git\mingw64\bin\git.exe"
-    "C:\Program Files\Git\mingw32\bin\git.exe"
-    "C:\Program Files (x86)\Git\cmd\git.exe"
-    "C:\Program Files (x86)\Git\bin\git.exe"
-    "C:\Program Files (x86)\Git\mingw64\bin\git.exe"
-    "C:\Program Files (x86)\Git\mingw32\bin\git.exe"
-) DO (
-    IF EXIST %%I (
-        SET "GIT_EXECUTABLE=%%I"
-        GOTO GitFound
-    )
-)
-
-:GitFound
-IF NOT DEFINED GIT_EXECUTABLE (
-    echo Git not found. Please install Git or update the script with the correct path.
-    exit /b 1
-) ELSE (
-    echo Found Git executable: %GIT_EXECUTABLE%
-)
-
-
-
-@REM set RPath=%USERPROFILE%\Documents\R\R-4.1.2\bin
-
 @echo off
 SETLOCAL
 
@@ -54,26 +25,6 @@ IF NOT DEFINED RPath (
     echo R directory not found.
 ) ELSE (
     echo Found R directory: %RPath%
-)
-
-
-
- 
-REM Check if the folder is already a Git repository
-IF EXIST "%CD%\.git" (
-    echo This is already a Git repository.
-    git pull origin
-) else (
-    echo This is not a Git repository. Initializing a new one...
-    :: Initialize a new Git repository
-    git init
-
-    :: Add a remote origin pointing to a GitHub repository with the same name
-    set "githubRepo=https://github.com/FlowWest/bvr-wqx-uploader.git"
-    git remote add origin %githubRepo%
-
-    echo Initialized Git repository and added remote origin to:
-    echo %githubRepo%
 )
 
 
